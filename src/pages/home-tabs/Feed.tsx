e1faf5b77b9d81123521d388162cccc345acf96c
SinabiananRey <sinabiananrey@gmail.com>
2025-02-18 08:43:41 +0800

    added home-tabs

---
+++ src/pages/home-tabs/Feed.tsx
@@ -0,0 +1,28 @@
+import { 
+    IonButtons,
+      IonContent, 
+      IonHeader, 
+      IonMenuButton, 
+      IonPage, 
+      IonTitle, 
+      IonToolbar 
+  } from '@ionic/react';
+  
+  const Feed: React.FC = () => {
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot='start'>
+              <IonMenuButton></IonMenuButton>
+            </IonButtons>
+            <IonTitle>Feed</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent fullscreen>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%', }} > Favorites </div>
+        </IonContent>
+      </IonPage>
+    );
+  };
+ export default Feed;
