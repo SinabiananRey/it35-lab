53ed343036407bce26a276ee9249503e3b35b9ed
SinabiananRey <sinabiananrey@gmail.com>
2025-02-18 08:44:52 +0800

    Updates

--- assets/index3-legacy-D2jDH33r.js
+++ assets/index3-legacy-D2jDH33r.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Bq7LaQ6U.js"],(function(e,t){"use strict";var r;return{setters:[t=>{r=t.G,e("GESTURE_CONTROLLER",t.G)}],execute:function(){
+System.register(["./index-legacy-DgpzRZcU.js"],(function(e,t){"use strict";var r;return{setters:[t=>{r=t.G,e("GESTURE_CONTROLLER",t.G)}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-dTjJFE-4.js
+++ assets/swipe-back-legacy-dTjJFE-4.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Bq7LaQ6U.js","./index3-legacy-BcmnvbWb.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-DgpzRZcU.js","./index3-legacy-D2jDH33r.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
