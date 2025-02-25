a6682c466ab12907fa480fdc39d351c43c1b64d6
taloy2 <sinabiananrey@gmail.com>
2025-02-25 10:01:58 +0800

    Updates

--- assets/ios.transition-legacy-D66k1IzI.js
+++ assets/ios.transition-legacy-D66k1IzI.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Fspp480C.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-BReFVgo-.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/keyboard2-legacy-BbHms2fs.js
+++ assets/keyboard2-legacy-BbHms2fs.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Fspp480C.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-BReFVgo-.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
