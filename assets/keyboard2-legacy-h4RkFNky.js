a4eb6b2f25b4c37330c9399eb02c86d707fbf984
taloy2 <sinabiananrey@gmail.com>
2025-02-25 14:03:46 +0800

    Updates

--- assets/keyboard2-legacy-h4RkFNky.js
+++ assets/keyboard2-legacy-h4RkFNky.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Ba5chRM3.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-Cm6Qs5AA.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-BanIu1sw.js
+++ assets/swipe-back-legacy-BanIu1sw.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Ba5chRM3.js","./index3-legacy-COvJC5TW.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-Cm6Qs5AA.js","./index3-legacy-CFtyl6rJ.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
