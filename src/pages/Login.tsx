3858d37a61936ec9d9e6b0ee50945914a236627b
SinabiananRey <sinabiananrey@gmail.com>
2025-03-11 08:55:53 +0800

    new branch

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,27 +1,46 @@
 import { 
+  IonAlert,
   IonAvatar,
   IonButton,
-  IonButtons,
-    IonContent, 
-    IonHeader, 
-    IonIcon, 
-    IonInput, 
-    IonInputPasswordToggle, 
-    IonItem, 
-    IonMenuButton, 
-    IonPage, 
-    IonTitle, 
-    IonToolbar, 
-    useIonRouter
+  IonContent, 
+  IonHeader, 
+  IonIcon, 
+  IonInput, 
+  IonInputPasswordToggle, 
+  IonItem, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar, 
+  useIonRouter 
 } from '@ionic/react';
+import { useState } from 'react'; // Make sure to import useState
 import { logoIonic } from 'ionicons/icons';
 
 const Login: React.FC = () => {
   const navigation = useIonRouter();
+  const [showToast, setShowToast] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const user_email = 'hehe@gmail.com';
+  const user_pwd = '123';
 
   const doLogin = () => {
-      navigation.push('/it35-lab/app','forward','replace');
-  }
+    if(email !== user_email || password !== user_pwd) {
+      setShowAlert(true);
+      return;
+    } else {
+      console.log(email);
+      console.log(password);
+
+      setShowToast(true);
+      setTimeout(() => {
+        navigation.push('/it35-lab/app','forward','replace');
+      }, 1500);
+    }
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -30,22 +49,47 @@ const Login: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent className='ion-padding'>
-      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', }} > 
-      <IonAvatar>
-        <img alt="Silhouette of a person's head" src="https://ionicframework.com/docs/img/demos/avatar.svg" />  </IonAvatar>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '30%' }}>
+          <IonAvatar>
+            <img alt="Silhouette of a person's head" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
+          </IonAvatar>
+        </div>
 
-      <IonItem>
-<IonInput type="password" label="Password:" value="" placeholder="Password">
-<IonInputPasswordToggle slot="end"></IonInputPasswordToggle>
-  <IonInput label="Username:" placeholder="Username" >
-</IonInput>
-  </IonInput>
+        <IonItem>
+          <IonInput
+            type="email"
+            label="Username:"
+            value={email}
+            onIonChange={e => setEmail(e.detail.value!)}
+            placeholder="Username"
+          />
+        </IonItem>
+        
+        <IonItem>
+          <IonInput
+            type="password"
+            label="Password:"
+            value={password}
+            onIonChange={e => setPassword(e.detail.value!)}
+            placeholder="Password"
+          >
+            <IonInputPasswordToggle slot="end" />
+          </IonInput>
+        </IonItem>
 
-</IonItem>
-      </div>
-          <IonButton onClick={() => doLogin()} expand="full">
-              Login
-          </IonButton>
+        <IonButton onClick={() => doLogin()} expand="full">
+          Login
+        </IonButton>
+
+        {showAlert &&(
+          <IonAlert
+          isOpen={showAlert}
+          onDidDismiss={() => setShowAlert(false)}
+          header= "login failed"
+          message="username or password is incorrect"
+          buttons= {("OK")}
+          />
+        )}
       </IonContent>
     </IonPage>
   );
