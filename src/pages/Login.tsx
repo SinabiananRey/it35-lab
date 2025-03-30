59f692a939417229d4007cd4eaa4406f2cfdea12
taloy2 <sinabiananrey@gmail.com>
2025-03-30 09:11:35 +0800

    added user avatar

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -10,7 +10,7 @@ import {
   IonToast,  
   useIonRouter
 } from '@ionic/react';
-import { logoIonic } from 'ionicons/icons';
+import { IonImg } from '@ionic/react';
 import { useState } from 'react';
 import { supabase } from '../utils/supabaseClient';
 
@@ -68,19 +68,17 @@ const Login: React.FC = () => {
               height: '150px',
               borderRadius: '50%', 
               overflow: 'hidden' 
-            }}
-          >
-            <IonIcon 
-              icon={logoIonic}
-              color='primary'
-              style={{ fontSize: '120px', color: '#6c757d' }} 
-            />
+            }}>
+           <IonImg
+      src="https://freesvg.org/img/abstract-user-flat-4.png"
+      alt="user"
+    ></IonImg>
           </IonAvatar>
           <h1 style={{
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
-          }}>USER LOGIN</h1>
+          }}>LOGIN</h1>
           <IonInput
             label="Email" 
             labelPlacement="floating" 
@@ -102,10 +100,10 @@ const Login: React.FC = () => {
         </div>
         <IonButton onClick={doLogin} expand="full" shape='round'>
           Login
-        </IonButton>
+        </IonButton> 
 
         <IonButton routerLink="/it35-lab/register" expand="full" fill="clear" shape='round'>
-          Don't have an account? Register here
+        Register here!
         </IonButton>
 
         {/* Reusable AlertBox Component */}
