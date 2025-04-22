92947970882fba15ea0ba910efc310b8621ff450
SinabiananRey <sinabiananrey@gmail.com>
2025-04-22 08:34:50 +0800

    Updates

--- assets/swipe-back-legacy-3dHejsAt.js
+++ assets/swipe-back-legacy-3dHejsAt.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BUj-n0pu.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-BhkC9tw6.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
