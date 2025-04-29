2802f30016bb48919aa8c676a058a33f7bb2d268
SinabiananRey <sinabiananrey@gmail.com>
2025-04-29 08:51:20 +0800

    Updates

--- assets/md.transition-legacy-QzsyLLsB.js
+++ assets/md.transition-legacy-QzsyLLsB.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BhkC9tw6.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-z50wrNZX.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
