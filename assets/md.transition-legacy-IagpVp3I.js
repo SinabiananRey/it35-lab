c7ba342c941aa1446caedcf1a7c09590e836e13d
taloy2 <sinabiananrey@gmail.com>
2025-05-02 14:12:22 +0800

    Updates

--- assets/md.transition-legacy-IagpVp3I.js
+++ assets/md.transition-legacy-IagpVp3I.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-KcRmwEPZ.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-C6I5iEV4.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-B_eD-cBo.js
+++ assets/swipe-back-legacy-B_eD-cBo.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-KcRmwEPZ.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-C6I5iEV4.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
