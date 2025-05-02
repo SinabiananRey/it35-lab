d0a052ee7045f0b212e7e32bc73c00c9494ae0e9
taloy2 <sinabiananrey@gmail.com>
2025-05-02 13:53:14 +0800

    Updates

--- assets/ios.transition-legacy-BO1TzswB.js
+++ assets/ios.transition-legacy-BO1TzswB.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-2bJCSsu9.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-KcRmwEPZ.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
